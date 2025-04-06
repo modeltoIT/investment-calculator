769ef08ddbfaa0e6b21c049359e17f8b0dad30cf
Dmytro Ivasenko <dmtivasenko@gmail.com>
2025-04-06 19:55:04 +0400

    add table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
 import { FormComponent } from "./components/form/form.component";
+import { TableComponent } from "./components/table/table.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, FormComponent]
+  imports: [HeaderComponent, FormComponent, TableComponent]
 })
 export class AppComponent {}
